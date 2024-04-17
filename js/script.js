344646911632d8c763420b6287f25dc591607a5a
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-17 15:05:30 +0000

    save (Not the last changes !!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 George, Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George, Khalil
+// Created on: Apr 2024
 // This file contains the JS functions for index.html
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080, 
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page. 
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
