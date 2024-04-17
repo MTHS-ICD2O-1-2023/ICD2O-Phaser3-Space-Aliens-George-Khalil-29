8dc57dde6dbef648cbfd4d44d597843cdcfcc903
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-04-17 18:22:25 +0000

    save

--- js/game.js
+++ js/game.js
@@ -1,8 +1,10 @@
+/* global Phaser */
+
 // Copyright (c) 2024 George, Khalil All rights reserved
 //
 // Created by: George, Khalil
 // Created on: Apr 2024
-// This file contains the JS functions for index.html
+// This is the Phaser3 game configuration file
 
 /**
  * Start Phaser Game.
@@ -10,18 +12,18 @@
 const config = {
   type: Phaser.AUTO,
   width: 1920,
-  height: 1080, 
+  height: 1080,
   physics: {
     default: "arcade",
     arcade: {
       debug: true,
     },
   },
-  // set background color
+  //set background color
   backgroundColor: 0x5f6e7a,
   scale: {
     mode: Phaser.Scale.FIT,
-    // we place it in the middle of the page. 
+    // we place it in the middle of the page.
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
 }
