c6134d6b51989c87cb2433d43b14524f4d076e96
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-11 14:29:51 +0000

    save

--- js/gameScene.js
+++ js/gameScene.js
@@ -18,6 +18,7 @@ class GameScene extends Phaser.Scene {
 
     this.background = null 
     this.ship = null
+    this.fireMissile = false
   }
 
 /**
@@ -37,8 +38,9 @@ class GameScene extends Phaser.Scene {
   preload() {
     console.log("Game Scene")
 
-    this.load.image("startBackground", "./assets/startBackground.png")
+    this.load.image("starBackground", "./assets/starBackground.png")
     this.load.image("ship", "./assets/spaceShip.png")
+    this.load.image("missile", "assets/missile.png")
   }
 
   /**
@@ -47,10 +49,12 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create(data) {
-   this.background = this.add.image(0, 0, "startBackground").setScale(2.0)
+   this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
    this.background.setOrigin(0, 0)
 
    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+   this.missileGroup = this.physics.add.group()
 }
 
   /**
@@ -62,6 +66,7 @@ class GameScene extends Phaser.Scene {
    update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey("LEFT")
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
@@ -75,6 +80,18 @@ class GameScene extends Phaser.Scene {
       if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
+    }
+
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
+        this.missileGroup.add(aNewMissile)
+      }
+    }
+
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
     }
    }
  }
