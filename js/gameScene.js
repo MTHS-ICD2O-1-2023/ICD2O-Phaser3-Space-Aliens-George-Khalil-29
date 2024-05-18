9afc23a8360ad4f37790c985db438e80c0bd0730
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-05-18 20:25:45 +0000

    save

--- js/gameScene.js
+++ js/gameScene.js
@@ -6,10 +6,8 @@
 // Created on: Apr 2024
 // This is the Phaser3 game configuration file
 
-/**
- * This class is the Menu Scene.
- */
 class GameScene extends Phaser.Scene {
+
   // create an alien
   createAlien () {
   const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
@@ -29,6 +27,9 @@ class GameScene extends Phaser.Scene {
     this.background = null
     this.ship = null
     this.fireMissile = false
+    this.score = 0
+    this.scoreText = null
+    this.scoreTextStyle = { font: '65px Arial', fill: '#ffffff', algin: 'center'}
   }
 
   /**
@@ -67,6 +68,8 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
+    this.scoreText = this.add.text(10, 10, 'Score: ' + this.score.toString(), this.scoreTextStyle)
+
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
     //create a group for the missiles
@@ -81,6 +84,8 @@ class GameScene extends Phaser.Scene {
     alienCollide.destroy()
     missileCollide.destroy()
     this.sound.play('explosion')
+    this.score = this.score + 1
+    this.scoreText.setText('Score: ' + this.score.toString())
     this.createAlien()
     this.createAlien()
     }.bind(this))
